df3a135959852028659b51de7548ee5b880e9105
navigator085 <gh.nyjvw@slmails.com>
2025-05-07 01:04:13 +0000

    Update ollama.sh
    remendo do remendo 2

--- ct/ollama.sh
+++ ct/ollama.sh
@@ -26,8 +26,7 @@ function update_script() {
     msg_error "No Ollama Installation Found!"
     exit
   fi
-#  RELEASE=$(curl -fsSL https://api.github.com/repos/ollama/ollama/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
-  RELEASE=$(v0.6.8)
+  RELEASE=v0.6.8
   if [[ ! -f /opt/Ollama_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/Ollama_version.txt)" ]]; then
     if [[ ! -f /opt/Ollama_version.txt ]]; then
       touch /opt/Ollama_version.txt
